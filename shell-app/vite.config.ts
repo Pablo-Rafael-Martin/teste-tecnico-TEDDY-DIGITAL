c6dabad3632553f15d684655c26eaa18d7d7ddfe
Pablo Rafael Caramori Fontes Martin <pablo.rafael.cfm@gmail.com>
2025-08-21 16:54:46 -0300

    Adicionando env para definir o local dos MFEs de acordo com o ambiente prod / dev

--- shell-app/vite.config.ts
+++ shell-app/vite.config.ts
@@ -3,20 +3,20 @@ import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		federation({
-			name: "shell_app",
-			remotes: {
-				clientes_mfe: "http://localhost:5001/assets/remoteEntry.js",
-			},
-			shared: ["react", "react-dom", "react-router-dom"],
-		}),
-	],
-	build: {
-		modulePreload: false,
-		target: "esnext",
-		minify: false,
-		cssCodeSplit: false,
-	},
+    plugins: [
+        react(),
+        federation({
+            name: "shell_app",
+            remotes: {
+                clientes_mfe: `${process.env.VITE_CLIENTES_MFE_URL}/remoteEntry.js`,
+            },
+            shared: ["react", "react-dom", "react-router-dom"],
+        }),
+    ],
+    build: {
+        modulePreload: false,
+        target: "esnext",
+        minify: false,
+        cssCodeSplit: false,
+    },
 });
