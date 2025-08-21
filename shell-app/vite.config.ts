b82281d6c3e17239613d53948d74044346939fc5
Pablo Rafael Caramori Fontes Martin <pablo.rafael.cfm@gmail.com>
2025-08-21 17:49:11 -0300

    fix: env

--- shell-app/vite.config.ts
+++ shell-app/vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-    plugins: [
-        react(),
-        federation({
-            name: "shell_app",
-            remotes: {
-                clientes_mfe: "http://localhost:5001/assets/remoteEntry.js",
-            },
-            shared: ["react", "react-dom", "react-router-dom"],
-        }),
-    ],
-    build: {
-        modulePreload: false,
-        target: "esnext",
-        minify: false,
-        cssCodeSplit: false,
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            react(),
+            federation({
+                name: "shell_app",
+                remotes: {
+                    clientes_mfe: `${env.VITE_CLIENTES_MFE_URL}/assets/remoteEntry.js`,
+                },
+                shared: ["react", "react-dom", "react-router-dom"],
+            }),
+        ],
+        build: {
+            modulePreload: false,
+            target: "esnext",
+            minify: false,
+            cssCodeSplit: false,
+        },
+    };
 });
